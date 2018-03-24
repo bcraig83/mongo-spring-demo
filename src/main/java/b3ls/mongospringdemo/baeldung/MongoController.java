2d7eaa05f8c156b6508fb7026307a97da1bd2948
Ben Craig <bcraig83@gmail.com>
2018-03-24 15:07:10 +0000

    Just fill out a simple welcome message to prove the controller is wired up properly.

--- src/main/java/b3ls/mongospringdemo/baeldung/MongoController.java
+++ src/main/java/b3ls/mongospringdemo/baeldung/MongoController.java
@@ -1,5 +1,13 @@
 package b3ls.mongospringdemo.baeldung;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MongoController {
 
+  @RequestMapping("/")
+  public String helloWorld() {
+    return "Hello World!";
+  }
 }
